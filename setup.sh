4a756c8b002b9540ab49025240a54bb20b45b61d
KyoungjunPark <parkkjun525@gmail.com>
2025-05-05 17:33:21 -0500

    Commit

--- setup.sh
+++ setup.sh
@@ -6,8 +6,8 @@ pip install -e ".[dev]"
 pip install wandb==0.18.3
 pip install tensorboardx
 pip install qwen_vl_utils torchvision
-# pip install flash-attn --no-build-isolation
-pip install flash-attn==2.5.7 --no-build-isolation
+pip install flash-attn --no-build-isolation
+
 # vLLM support 
 pip install vllm==0.7.2
 
